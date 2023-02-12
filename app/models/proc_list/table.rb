b0d08a1656a51f54117645ec60365f4a554aaba7
olha <olhavasilieva@gmail.com>
2023-02-12 12:46:13 +0100

    ruby_version

--- app/models/proc_list/table.rb
+++ app/models/proc_list/table.rb
@@ -11,23 +11,41 @@ module ProcList
       @processes = []
     end
 
-    def refresh!
-      new_processes = Process.list
-      new_process_pids = new_processes.map(&:pid)
-      existing_process_pids = @processes.map(&:pid)
+#     def refresh!
+#       new_processes = Process.list
+#       new_process_pids = new_processes.map(&:pid)
+#       existing_process_pids = @processes.map(&:pid)
       
-      terminated_process_pids = existing_process_pids - new_process_pids
-      @processes.reject! { |p| terminated_process_pids.include?(p.pid) }
+#       terminated_process_pids = existing_process_pids - new_process_pids
+#       @processes.reject! { |p| terminated_process_pids.include?(p.pid) }
       
-      new_processes.each do |process|
-        index = @processes.find_index { |p| p.pid == process.pid }
-        if index
-          @processes[index] = process
-        else
-          @processes << process
-        end
-      end
-      notify_observers
-    end
-  end
+#       new_processes.each do |process|
+#         index = @processes.find_index { |p| p.pid == process.pid }
+#         if index
+#           @processes[index] = process
+#         else
+#           @processes << process
+#         end
+#       end
+#       notify_observers
+#     end
+#   end
+# end
+
+def refresh!
+  new_processes_list = Process.list
+
+  new_pids = new_processes_list.map(&:pid)
+  old_pids = @processes.map(&:pid)
+
+  return false if (new_pids & old_pids) == new_pids
+
+  @processes.select! { |p| new_pids.include?(p.pid) }
+  new_processes_list.reject! { |p| old_pids.include?(p.pid) }
+
+  @processes += new_processes_list
+
+  @processes.sort_by!(&:pid)
+  notify_observers
+  true
 end
